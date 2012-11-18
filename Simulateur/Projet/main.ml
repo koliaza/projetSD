7f50747f425d88b81d8da8e6eb304973857200ed
koliaza <kostat75@gmail.com>
2012-11-18 03:59:18 +0100

    test avant compilation
    Plus de compilateur fonctionnel ici, donc passage sur ordi de tito.

--- Simulateur/Projet/main.ml
+++ Simulateur/Projet/main.ml
@@ -2,24 +2,37 @@
 (* definition des options *)
 open Def
 				
-let default = {oprint = true; oschedule = false; odebug = false;
-			   overbose = false; osteps = -1; oclock = 1.}
-(* on modifie le default au fur et a mesure *)
+let options = {oprint = true; oschedule = false; odebug = false; 
+			   overbose = false; osteps = -1; oclock = 1.; 
+			   ramsize=256; romsize=256; ramlist = [] ;
+			   ram_file=""; rom_file=""}
+(* on modifie les options au fur et a mesure *)
 (*fin des options *)
 
 let option_list = 
-    ["-scheduleonly", Arg.Unit (fun () -> default.oschedule <- true),
+    ["-scheduleonly", Arg.Unit (fun () -> options.oschedule <- true),
                      "Stops after printing the result of scheduling" ;
-	"-noprint", Arg.Unit (fun () -> default.oprint <- false),
+	 "-noprint", Arg.Unit (fun () -> options.oprint <- false),
                 "Does not print the result of scheduling";
-	"-debug", Arg.Unit (fun () -> default.odebug <- true),
+	 "-debug", Arg.Unit (fun () -> options.odebug <- true),
               "prints step by step and waits for user control"; 
-     "-n", Arg.Int (fun x -> default.osteps <- x),
+	 "-n", Arg.Int (fun x -> options.osteps <- x),
            "Number of steps to simulate";
-	 "-clock", Arg.Float (fun x -> default.oclock <- x),
+	 "-clock", Arg.Float (fun x -> options.oclock <- x),
                "clock speed to simulate";
-	 "-verbose", Arg.Unit (fun () -> default.overbose <- true),
-                 "print all the outputs"]
+	 "-verbose", Arg.Unit (fun () -> options.overbose <- true),
+                 "print all the outputs"
+	 "-ramsize", Arg.Int  (fun x -> options.ramsize <- x),
+				 "Size (in word) of the ram array";
+	 "-romsize", Arg.Int  (fun x -> options.ramsize <- x),
+				 "Size (in word) of the rom array";
+	 "-raminspect", Arg.List (fun l -> options.ramlist <- l),
+				 "IntList of the ram adresses to follow"
+	 "-ramfile", Arg.String (fun s -> options.ram_file <- s)
+				 "adress of the ram file"
+	 "-romfile", Arg.String (fun s -> options.ram_file <- s)
+				 "adress of the rom file"
+				 ]
    
 let main_exec filename= 
     let netp = Netlist.read_file filename in
@@ -30,14 +43,17 @@ let main_exec filename=
     in
     begin try
         let p = Scheduler.schedule netp in
-		if default.oprint then
-			Netlist_printer.print_program out p; (* Netlist_printer est à modifier pour indiquer comment est traité le cas des registres *)
-		if not default.oschedule then (
+		if options.oprint then
+			Netlist_printer.print_program out p; 
+			(* Netlist_printer est à modifier pour indiquer comment est traité le cas des registres *)
+		if not options.oschedule then (
+			let ram = Dataio.readram options in
+			let rom = Dataio.readrom options in
 			let mp = conversion_programme p in
-				 if default.odebug then
-				  Execution.exec_debug mp default  (* koliaza: Marc veut que tu lui passes une variable de plus pour afficher les sorties : regarde son code aux lignes où il y a !Main. *)
+				 if options.odebug then
+				  Execution.exec_debug mp options ram rom  
 				else
-				  Execution.execution  mp default
+				  Execution.execution  mp options ram rom
 			) 
 	  with        
 		| Scheduler.Combinational_cycle ->
@@ -49,4 +65,4 @@ let main_exec filename=
 
 let main () = Arg.parse option_list main_exec ""
 
-let _ = main ()
+let _ = main () 
